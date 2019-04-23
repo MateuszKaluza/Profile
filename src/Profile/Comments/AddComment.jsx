d82358317efc6bc2c5a9620b8ce322a44a924093
Mateusz Kaluza <Mateusz.Kaluza@ad.infosys.com>
2019-04-23 11:58:09 +0200

    Finish layout

--- src/Profile/Comments/AddComment.jsx
+++ src/Profile/Comments/AddComment.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import './AddComment.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 class AddComment extends Component {
     render() {
         return (
             <div>
-                Add a comment
+                <textarea className="AddComment" type="text" placeholder="Add a comment" />
+                <FontAwesomeIcon icon={faPaperPlane} size="sm" className="SendIcon" title="Send"/>
             </div>
         );
     };
